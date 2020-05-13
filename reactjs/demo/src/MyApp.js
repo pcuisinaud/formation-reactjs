eb695311acf1b93f1173dee6c443e2515f5cc091
Philippe CUISINAUD <philippe.cuisinaud@docapost.fr>
2020-05-13 15:18:18 +0200

    component embarqué

--- reactjs/demo/src/MyApp.js
+++ reactjs/demo/src/MyApp.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 function MyApp() {
     const title = "Simple titre";
+
+    let list = <ul>
+        <li>Element 1</li>
+        <li>Element 2</li>
+        <li>Element 3</li>
+    </ul>
+
     return (
         <div>
             <button onClick={()=>console.log('ok')}>Click</button>
             <div>{title}</div>
+            {list}
         </div>
     );
 }
