ff35de9981f7d779b62e95e5489db86f34660bb4
Philippe CUISINAUD <philippe.cuisinaud@docapost.fr>
2020-05-14 12:21:35 +0200

    props

--- reactjs/demo/src/PlayerList.js
+++ reactjs/demo/src/PlayerList.js
@@ -2,14 +2,8 @@ import React from 'react';
 
 class PlayerList extends React.Component {
 
-    constructor(params) {
-        super(params);
-        console.log(params);
-    }
-
     render() {
-        console.log(this.params)
-        const players = this.params.players.map((p) => (<p>
+        const players = this.props.players.map((p) => (<p>
             <span>Joueur : {p.name}</span>
             <span>Equipe : {p.team}</span>
         </p>))
