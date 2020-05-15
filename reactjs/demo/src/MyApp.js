d39a446a3005875705fc3440c8af985e45938c5d
Philippe CUISINAUD <philippe.cuisinaud@docapost.fr>
2020-05-15 10:20:29 +0200

    Router

--- reactjs/demo/src/MyApp.js
+++ reactjs/demo/src/MyApp.js
@@ -4,17 +4,38 @@ import { LangProvider } from './LangContext';
 import LangSwitcher from './LangSwitcher';
 import Test from './Test';
 import SubTest from './SubTest';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function MyApp() {
     return (
         <LangProvider value='fr'>
-            <Test title="titre" content="Mon Contenu">
-                <SubTest/>
-                <SubTest/>                
-                <SubTest/>
-            </Test>
-            <LangSwitcher/>
-            <PlayerManager />
+            <LangSwitcher />
+            <Router>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/test">Test</Link></li>
+                        <li><Link to="/player">Player</Link></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/test">
+                        <Test title="titre" content="Mon Contenu">
+                            <SubTest />
+                            <SubTest />
+                            <SubTest />
+                        </Test>
+                    </Route>
+                    <Route path="/player">
+                        <PlayerManager />
+                    </Route>
+                </Switch>
+            </Router>
         </LangProvider>
     );
 }
