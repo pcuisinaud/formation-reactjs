8765fc6b6675801937b7c06e3532b5b50d312c73
Philippe CUISINAUD <philippe.cuisinaud@docapost.fr>
2020-05-15 11:52:46 +0200

    package server

--- reactjs/server/server.js
+++ reactjs/server/server.js
@@ -3,8 +3,11 @@ const http = require('http');
 const teams = ["Juve", "Strasbourg", "Madrid"]
 
 http.createServer((req, res) => {
+    console.log(req.url);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.write(JSON.stringify(teams));
+    if (req.url == '/teams') {
+        res.write(JSON.stringify(teams));
+    }
     res.end();
 }).listen(8080);
 console.log('Server running on 8080');
